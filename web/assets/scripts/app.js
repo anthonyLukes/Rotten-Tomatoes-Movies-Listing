a0d53776bd2ddc53d4fa7aefcd3dd3b909358923
Anthony Lukes <anthony.lukes@gmail.com>
2014-08-19 21:07:40 -0500

    upated ng-class to be implemented a lot cleaner with logic in controller and updated controller to use variables all tied to the

--- web/assets/scripts/app.js
+++ web/assets/scripts/app.js
@@ -2,25 +2,46 @@
     var app = angular.module('movieApp', []);
 
     app.controller('MoviesController', ['$http','$scope', function($http,$scope) {
-        var self = this;
-
-        this.movies = [];
-        this.movieSort = "ratings.critics_score";
-        this.moviesLimit = 10;
+        $scope.movies = [];
+        $scope.movieSort = "ratings.critics_score";
+        $scope.moviesLimit = 10;
 
         // copy initial values to temp variables
         // to populate values in the UI
-        this.movieSortTemp = this.movieSort;
-        this.moviesLimitTemp = this.moviesLimit;
+        $scope.movieSortTemp = $scope.movieSort;
+        $scope.moviesLimitTemp = $scope.moviesLimit;
 
         $scope.updateMovies = function() {
-            self.movieSort = self.movieSortTemp;
-            self.moviesLimit = self.moviesLimitTemp;
+            $scope.movieSort = $scope.movieSortTemp;
+            $scope.moviesLimit = $scope.moviesLimitTemp;
+        };
+
+        $scope.getRatingCssClass = function(rating) {
+            switch (rating) {
+                case "Certified Fresh":
+                    return "rating_certifiedFresh";
+                    break;
+                case "Fresh":
+                    return "rating_fresh";
+                    break;
+                case "Rotten":
+                    return "rating_rotten";
+                    break;
+                case "Spilled":
+                    return "rating_spilled";
+                    break;
+                case "Unknown":
+                    return "rating_unknown";
+                    break;
+                case "Upright":
+                    return "rating_upright";
+                    break;
+            };
         };
 
         // get & set movies from jsonp call
         $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=gz5uhgtn2my2zy7dsfmnjyrb&page_limit=25&callback=JSON_CALLBACK').success(function(data) {
-            self.movies = data.movies;
+            $scope.movies = data.movies;
         });
     }]);
 })();
