5c54d66074f97086ebcc2b24f214f0b49f633d78
Anthony Lukes <alukes@nerdery.com>
2014-08-20 21:33:36 -0500

    updated app to use objects for display properties and had to use angular.copy to keep the temporary object and object that the ng-repeat is generated from from not referencing each other directly. Also, applied update to json so that release date matched the json from the live feed. Need to create a service for the json and could normalize from there instead

--- web/assets/scripts/app.js
+++ web/assets/scripts/app.js
@@ -1,19 +1,56 @@
 (function() {
-    var app = angular.module('movieApp', []);
+    var app = angular.module("movieApp", []);
 
-    app.controller('MoviesController', ['$http','$scope', function($http,$scope) {
+    app.filter("myFilter", function(){
+        return function(movies, sortBy) {
+            var filteredMovies = []
+            for (var i = 0; i < movies.length; i++) {
+                var movie = movies[i];
+
+                if (sortBy === "ratings.critics_score" || sortBy === "-ratings.critics_score") {
+                    if (movie.ratings) {
+                        if (movie.ratings.critics_rating) {
+
+                            filteredMovies.push(movie);
+                        }
+                    }
+
+                } else if (sortBy === "ratings.audience_score" || sortBy === "-ratings.audience_score") {
+                    if (movie.ratings) {
+                        if (movie.ratings.critics_rating && movie.ratings.audience_rating) {
+
+                            filteredMovies.push(movie);
+                        }
+                    }
+
+                } else {
+                    filteredMovies.push(movie);
+                }
+            };
+
+            return filteredMovies;
+        };
+    });
+
+    app.controller("MoviesController", ["$http","$scope", function($http,$scope) {
         $scope.movies = [];
-        $scope.movieSort = "ratings.critics_score";
-        $scope.moviesLimit = 10;
 
-        // copy initial values to temp variables
-        // to populate values in the UI
-        $scope.movieSortTemp = $scope.movieSort;
-        $scope.moviesLimitTemp = $scope.moviesLimit;
+        $scope.displayOptions = {
+            movieSort: "ratings.critics_score",
+            moviesLimit: 10
+        };
+
+        $scope.displayOptionsTemporary = {};
+
+        // Copy initial values to temp variables
+        // to populate values in the UI.
+        //
+        // Need to use objects to bind to the model in ng-include and need
+        // to use angular copy to not bind the objects together.
+        angular.copy($scope.displayOptions, $scope.displayOptionsTemporary);
 
         $scope.updateMovies = function() {
-            $scope.movieSort = $scope.movieSortTemp;
-            $scope.moviesLimit = $scope.moviesLimitTemp;
+            angular.copy($scope.displayOptionsTemporary, $scope.displayOptions);
         };
 
         $scope.getRatingCssClass = function(rating) {
@@ -44,7 +81,7 @@
         //     $scope.movies = data.movies;
         // });
 
-        $http.get('assets/data/movies.json').success(function(data) {
+        $http.get("assets/data/movies.json").success(function(data) {
             $scope.movies = data.movies;
         });
     }]);
