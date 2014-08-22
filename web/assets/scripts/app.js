bf97fa3d92d0271767498e7c60d968de8e8429e8
Anthony Lukes <alukes@nerdery.com>
2014-08-21 23:14:49 -0500

    updated comment styles and updated formatting a bit to app.js

--- web/assets/scripts/app.js
+++ web/assets/scripts/app.js
@@ -10,19 +10,15 @@
                 if (sortBy === "ratings.critics_score" || sortBy === "-ratings.critics_score") {
                     if (movie.ratings) {
                         if (movie.ratings.critics_rating) {
-
                             filteredMovies.push(movie);
                         }
                     }
-
                 } else if (sortBy === "ratings.audience_score" || sortBy === "-ratings.audience_score") {
                     if (movie.ratings) {
                         if (movie.ratings.critics_rating && movie.ratings.audience_rating) {
-
                             filteredMovies.push(movie);
                         }
                     }
-
                 } else {
                     filteredMovies.push(movie);
                 }
@@ -38,10 +34,10 @@
 
                 var deferred = $q.defer();
 
-                $http.get("assets/data/movies.json").success(function(data){
-                    deferred.resolve(data);
-                }).error(function(data){
-                    deferred.reject(data);
+                $http.get("assets/data/movies.json").success(function(moviesResponse){
+                    deferred.resolve(moviesResponse);
+                }).error(function(response){
+                    deferred.reject(response);
                 });
 
 
@@ -68,68 +64,84 @@
 
                 this.isTabActive = function(tabNumber) {
                     return tabNumber === this.activeTab;
-                }
+                };
             },
             controllerAs: 'tab'
         }
     });
 
-    app.controller("MoviesController", ["$http","$scope", "moviesProvider", function($http,$scope,moviesProvider) {
-        $scope.movies = [];
-        $scope.movieLoadingError = false;
+    app.controller("MoviesController", [
+        "$http",
+        "$scope",
+        "moviesProvider",
+        function(
+            $http,
+            $scope,
+            moviesProvider
+        ) {
+            $scope.movies = [];
+            $scope.movieLoadingError = false;
 
-        $scope.displayOptions = {
-            movieSort: "ratings.critics_score",
-            moviesLimit: 10
-        };
-
-        $scope.displayOptionsTemporary = {};
-
-        // Copy initial values to temp variables
-        // to populate values in the UI.
-        //
-        // Need to use objects to bind to the model in ng-include and need
-        // to use angular copy to not bind the objects together.
-        angular.copy($scope.displayOptions, $scope.displayOptionsTemporary);
-
-        $scope.updateMovies = function() {
-            angular.copy($scope.displayOptionsTemporary, $scope.displayOptions);
-        };
-
-        $scope.getRatingCssClass = function(rating) {
-            switch (rating) {
-                case "Certified Fresh":
-                    return "rating_certifiedFresh";
-                    break;
-                case "Fresh":
-                    return "rating_fresh";
-                    break;
-                case "Rotten":
-                    return "rating_rotten";
-                    break;
-                case "Spilled":
-                    return "rating_spilled";
-                    break;
-                case "Unknown":
-                    return "rating_unknown";
-                    break;
-                case "Upright":
-                    return "rating_upright";
-                    break;
+            $scope.displayOptions = {
+                movieSort: "ratings.critics_score",
+                moviesLimit: 10
             };
-        };
 
-        // get & set movies from jsonp call
-        // $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=gz5uhgtn2my2zy7dsfmnjyrb&page_limit=25&callback=JSON_CALLBACK').success(function(data) {
-        //     $scope.movies = data.movies;
-        // });
+            $scope.displayOptionsTemporary = {};
 
-        moviesProvider.getMovies().then(function(data) {
-            // success
-            $scope.movies = data.movies;
-        }, function(reason) {
-            // error
-            $scope.movieLoadingError = true;
-        });
-    }]);
+            /**
+             * Copy initial values to temp variables
+             * to populate values in the UI.
+             *
+             * Need to use objects to bind to the model in ng-include and need
+             * to use angular copy to not bind the objects together.
+             *
+             */
+            angular.copy($scope.displayOptions, $scope.displayOptionsTemporary);
+
+            $scope.updateMovies = function() {
+                angular.copy($scope.displayOptionsTemporary, $scope.displayOptions);
+            };
+
+            $scope.getRatingCssClass = function(rating) {
+                switch (rating) {
+                    case "Certified Fresh":
+                        return "rating_certifiedFresh";
+                        break;
+                    case "Fresh":
+                        return "rating_fresh";
+                        break;
+                    case "Rotten":
+                        return "rating_rotten";
+                        break;
+                    case "Spilled":
+                        return "rating_spilled";
+                        break;
+                    case "Unknown":
+                        return "rating_unknown";
+                        break;
+                    case "Upright":
+                        return "rating_upright";
+                        break;
+                };
+            };
+
+
+            // $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=gz5uhgtn2my2zy7dsfmnjyrb&page_limit=25&callback=JSON_CALLBACK').success(function(data) {
+            //     $scope.movies = data.movies;
+            // });
+
+
+            /**
+             * get & set movies from jsonp call
+             */
+            moviesProvider.getMovies().then(function(moviesResponse) {
+                // success
+                $scope.movies = moviesResponse.movies;
+            }, function(reason) {
+                // error
+                $scope.movieLoadingError = true;
+            });
+        }
+    ]);
 })();
