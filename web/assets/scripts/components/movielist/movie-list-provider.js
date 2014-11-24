d06bba33575cda456ef07f066592c1491bb7cf61
Anthony Lukes <alukes@nerdery.com>
2014-11-23 21:55:31 -0600

    updating movie-list-provider to pull from live jsonp api

--- web/assets/scripts/components/movielist/movie-list-provider.js
+++ web/assets/scripts/components/movielist/movie-list-provider.js
@@ -4,7 +4,12 @@ app.factory('movieListProvider', function($q, $http) {
 
             var deferred = $q.defer();
 
-            $http.get("assets/data/movies.json").success(function(data){
+            // $http.get("assets/data/movies.json").success(function(data){
+            //     deferred.resolve(data);
+            // }).error(function(data){
+            //     deferred.reject(data);
+            // });
+            $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=gz5uhgtn2my2zy7dsfmnjyrb&page_limit=25&callback=JSON_CALLBACK').success(function(data) {
                 deferred.resolve(data);
             }).error(function(data){
                 deferred.reject(data);
